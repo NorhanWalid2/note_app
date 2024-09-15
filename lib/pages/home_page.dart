4249dcd11424792bc8007a952e27d0fe5fde29bc
NorhanWalid2 <walidnorhan2@gmail.com>
2024-09-16 01:35:41 +0300

    show model button sheet

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/pages/widgets/add_note.dart';
 //import 'package:notes_app/pages/widgets/notes_view_body';
 import 'package:notes_app/pages/widgets/notes_view_body.dart';
 //import 'package:notes_app/widgets/notes_view_body';
@@ -10,7 +11,13 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return AddNoteButtomSheet();
+              });
+        },
         child: Icon(Icons.add),
         shape: CircleBorder(),
       ),
