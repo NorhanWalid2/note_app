8f5194f80fe2b0a4a0e0142fcf917d36fa86fa9c
NorhanWalid2 <walidnorhan2@gmail.com>
2024-09-17 18:59:19 +0300

    add add note cubit to add note buttom sheet

--- lib/pages/widgets/add_note.dart
+++ lib/pages/widgets/add_note.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_app/pages/widgets/custom_button.dart';
 import 'package:notes_app/pages/widgets/custom_text_field.dart';
 
@@ -10,7 +14,21 @@ class AddNoteButtomSheet extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: SingleChildScrollView(
-        child: FormAddNote(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNotesFailer) {
+              print('failed ${state.errorMessage}');
+            }
+            if (state is AddNotesSuccessful) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: state is AddNotesLoading ? true : false,
+                child: FormAddNote());
+          },
+        ),
       ),
     );
   }
